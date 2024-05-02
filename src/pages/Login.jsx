0346ca63f96e12fd4f23d960ef33c277ffedd069
riyadhbd2 <riyadhbd2@gmail.com>
2024-05-02 23:01:16 +0200

    login and sign up form created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,23 @@
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="container mx-auto flex justify-center mt-52 border border-blue-500 w-52">
+
+            <form>
+                <h1 className="text-center text-orange-400 text-2xl">Login</h1>
+                <div>
+                    <h1>Name:</h1>
+                    <input className="border border-green-500" type="text" name="name" id="name" placeholder="Name"/>
+                </div>
+                <div>
+                    <h1>Email:</h1>
+                    <input className="border border-green-500" type="email" name="email" id="email" placeholder="Email" />
+                </div>
+                <div className="text-center pb-4">
+                    <button className="text-white bg-orange-400 rounded-lg p-1 text-sm" type="submit">Sign In</button>
+                </div>
+            </form>
+            
         </div>
     );
 };
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,9 +2,26 @@
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+      <div className="container mx-auto flex justify-center mt-52 border border-blue-500 w-52">
+          <form>
+                <h1 className="text-center text-orange-400 text-2xl">Sign Up</h1>
+                <div>
+                    <h1>Name:</h1>
+                    <input className="border border-green-500" type="text" name="name" id="name" placeholder="Name"/>
+                </div>
+                <div>
+                    <h1>Email:</h1>
+                    <input className="border border-green-500" type="email" name="email" id="email" placeholder="Email" />
+                </div>
+                <div className="pb-2">
+                    <h1>Confirm Email:</h1>
+                    <input className="border border-green-500" type="email" name="email" id="email" placeholder="Repeat Email" />
+                </div>
+                <div className="text-center pb-4">
+                    <button className="text-white bg-orange-400 rounded-lg p-1 text-sm" type="submit">Sign Up</button>
+                </div>
+            </form>
+      </div>
     );
 };
 
