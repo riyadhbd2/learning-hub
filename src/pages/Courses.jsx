95fc8698dac89141f101bd1369538186bf242e4d
riyadhbd2 <riyadhbd2@gmail.com>
2024-05-09 08:50:05 +0200

    server data fetch in client side

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/courses')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
     return (
         <div className="container mx-auto">
-            <h1>All Courses are</h1>
+            <h1>All Courses are: {courses.length}</h1>
+            <div className="grid grid-cols-3">
+                {
+                    courses.map(course => <div className="border border-green-400 m-3 grid justify-center" key={course.id}>
+                        <p>{course.name}</p>
+                        <p>{course.duration}</p>
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
